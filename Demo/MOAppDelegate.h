78da6405b34501bc9c6e01421da5ad689090f91e
Ryan Jones <ryan@psychomomo.com>
2014-01-18 13:28:21 -0600

    Initial Commit

---
+++ Demo/MOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOAppDelegate.h
+//  Demo
+//
+//  Created by Ryan Jones on 1/18/14.
+//  Copyright (c) 2014 Psychomomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Ryan Jones on 1/18/14.
+//  Copyright (c) 2014 Psychomomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOAppDelegate class]));
+    }
+}
